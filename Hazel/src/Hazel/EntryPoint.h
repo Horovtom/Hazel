c919d9d133bd89ef1372c52db123eb23964ac242
Tomáš Hořovský <horovtom@fel.cvut.cz>
2019-08-27 23:26:30 +0200

    Adding premake build system

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
 	Hazel::Log::Init();
 	HZ_CORE_WARN("Initialized Log!");
 	int a = 5;
-	HZ_INFO("Hello! Var={0", a);
+	HZ_INFO("Hello! Var={0}", a);
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
---
+++ premake5.lua
@@ -0,0 +1,98 @@
+workspace "Hazel"
+    architecture "x64"
+
+    configurations {
+        "Debug",    -- Debug - everything enabled
+        "Release",  -- Debug with a lot of information stripped, optimizations turned on, but with enabled logging
+        "Dist"      -- Everything stripped, with no logging and optimized code
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    
+    includedirs {
+        "Hazel/vendor/spdlog/include",
+        "Hazel/src"
+    }
+
+    links {
+        "Hazel" -- It will link the Hazel project to this project (as a dependency)
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {
+            "HZ_PLATFORM_WINDOWS",
+            
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+        
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+
+project "Hazel"
+    location "Hazel" -- Ensure everything will be relative to the path
+    kind "SharedLib" -- Specify that it is a dll
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands { -- Copy compiled files to Sandbox
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+        
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+        
